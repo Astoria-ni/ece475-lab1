efb0dec50b228f10d1660a1430d3b7cc8248d134
Jishnu Roychoudhury <jr0664@adroit-vis.princeton.edu>
2026-02-09 01:10:40 -0500

    added signals

--- imuldiv/imuldiv-IntMulIterative.v
+++ imuldiv/imuldiv-IntMulIterative.v
@@ -20,21 +20,54 @@ module imuldiv_IntMulIterative
   input         mulresp_rdy
 );
 
+  wire          a_mux_sel;
+  wire          b_mux_sel;
+  wire          result_mux_sel;
+  wire          result_en;
+  wire          add_mux_sel;
+  wire          cntr_mux_sel;
+  wire          sign_en;
+  wire          sign_mux_sel;
+  wire          b_reg_lsb; //b_reg[0]
+  wire    [4:0] counter;
+
   imuldiv_IntMulIterativeDpath dpath
   (
     .clk                (clk),
     .reset              (reset),
     .mulreq_msg_a       (mulreq_msg_a),
     .mulreq_msg_b       (mulreq_msg_b),
-    .mulreq_val         (mulreq_val),
-    .mulreq_rdy         (mulreq_rdy),
     .mulresp_msg_result (mulresp_msg_result),
-    .mulresp_val        (mulresp_val),
-    .mulresp_rdy        (mulresp_rdy)
+    .a_mux_sel          (a_mux_sel),
+    .b_mux_sel          (b_mux_sel),
+    .result_mux_sel     (result_mux_sel),
+    .result_en          (result_en),
+    .add_mux_sel        (add_mux_sel),
+    .cntr_mux_sel       (cntr_mux_sel),
+    .sign_en            (sign_en),
+    .sign_mux_sel       (sign_mux_sel),
+    .b_reg_lsb          (b_reg_lsb),
+    .counter            (counter)
   );
 
   imuldiv_IntMulIterativeCtrl ctrl
   (
+    .clk                (clk),
+    .reset              (reset),
+    .mulreq_val         (mulreq_val),
+    .mulreq_rdy         (mulreq_rdy),
+    .mulresp_val        (mulresp_val),
+    .mulresp_rdy        (mulresp_rdy),
+    .a_mux_sel          (a_mux_sel),
+    .b_mux_sel          (b_mux_sel),
+    .result_mux_sel     (result_mux_sel),
+    .result_en          (result_en),
+    .add_mux_sel        (add_mux_sel),
+    .cntr_mux_sel       (cntr_mux_sel),
+    .sign_en            (sign_en),
+    .sign_mux_sel       (sign_mux_sel),
+    .b_reg_lsb          (b_reg_lsb),
+    .counter            (counter)
   );
 
 endmodule
@@ -50,14 +83,25 @@ module imuldiv_IntMulIterativeDpath
 
   input  [31:0] mulreq_msg_a,       // Operand A
   input  [31:0] mulreq_msg_b,       // Operand B
-  input         mulreq_val,         // Request val Signal
-  output        mulreq_rdy,         // Request rdy Signal
-
   output [63:0] mulresp_msg_result, // Result of operation
-  output        mulresp_val,        // Response val Signal
-  input         mulresp_rdy         // Response rdy Signal
+
+  input         a_mux_sel,
+  input         b_mux_sel,
+  input         result_mux_sel,
+  input         result_en,
+  input         add_mux_sel,
+  input         cntr_mux_sel,
+  input         sign_en,
+  input         sign_mux_sel,
+
+  output        b_reg_lsb,
+  output  [4:0] counter
 );
 
+  assign mulresp_msg_result = 64'b0;
+  assign b_reg_lsb          = 1'b0;
+  assign counter            = 5'b0;
+  /*
   //----------------------------------------------------------------------
   // Sequential Logic
   //----------------------------------------------------------------------
@@ -113,7 +157,7 @@ module imuldiv_IntMulIterativeDpath
 
   assign mulreq_rdy  = mulresp_rdy;
   assign mulresp_val = val_reg;
-
+  */
 endmodule
 
 //------------------------------------------------------------------------
@@ -122,7 +166,38 @@ endmodule
 
 module imuldiv_IntMulIterativeCtrl
 (
+  input         clk,
+  input         reset,
+
+  input         mulreq_val,
+  output        mulreq_rdy,
+  output        mulresp_val,
+  input         mulresp_rdy,
+
+  output        a_mux_sel,
+  output        b_mux_sel,
+  output        result_mux_sel,
+  output        result_en,
+  output        add_mux_sel,
+  output        cntr_mux_sel,
+  output        sign_en,
+  output        sign_mux_sel,
+
+  input         b_reg_lsb,
+  input   [4:0] counter
 );
+  
+  assign mulreq_rdy = 1'b1;
+  assign mulresp_val= 1'b0;
+
+  assign a_mux_sel  = 1'b0;
+  assign b_mux_sel  = 1'b0;
+  assign result_mux_sel = 1'b0;
+  assign result_en  = 1'b0;
+  assign add_mux_sel = 1'b0;
+  assign cntr_mux_sel = 1'b0;
+  assign sign_en     = 1'b0;
+  assign sign_mux_sel = 1'b0;
 
 endmodule
 
